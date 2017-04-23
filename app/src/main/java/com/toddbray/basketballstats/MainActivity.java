abc3231a9686ecdfa48dd8fddca165a6e3077926
Todd <Toddab16@gmail.com>
2017-04-23 10:49:26 -0400

    Added new player activity

--- app/src/main/java/com/toddbray/basketballstats/MainActivity.java
+++ app/src/main/java/com/toddbray/basketballstats/MainActivity.java
@@ -1,21 +1,26 @@
 package com.toddbray.basketballstats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DbDataSource dataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_stats);
+        setContentView(R.layout.activity_main);
 
         dataSource = new DbDataSource(getApplicationContext());
+        Button newPlayer = (Button) findViewById(R.id.add_player_button);
+        newPlayer.setOnClickListener(this);
     }
 
     @Override
@@ -51,5 +56,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         dataSource.close();
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_player_button:
+                Intent intent = new Intent (this, NewPlayer.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+        }
     }
 }
