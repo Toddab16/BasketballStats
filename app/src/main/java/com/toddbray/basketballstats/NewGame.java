366102ea384f42344241bbb63ad469215b90e5c9
Todd <Toddab16@gmail.com>
2017-04-24 14:14:56 -0400

    Changed Min SDK to 23 to handle time issues. Modified New game layout Finished adding new game logic

--- app/src/main/java/com/toddbray/basketballstats/NewGame.java
+++ app/src/main/java/com/toddbray/basketballstats/NewGame.java
@@ -1,81 +1,133 @@
 package com.toddbray.basketballstats;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
+
 
 /**
  * Created by Todd Desktop on 4/22/2017.
  */
 
-public class NewGame extends AppCompatActivity {
+public class NewGame extends AppCompatActivity implements View.OnFocusChangeListener{
 
     DbDataSource db = new DbDataSource(this);
-
+    Date date;
+    Date [] times = new Date[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_game);
-
-
-        Button saveButton = (Button) findViewById(R.id.game_save_button);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                EditText et = (EditText)findViewById(R.id.date_editText);
-                String date = et.getText().toString();
-                et = (EditText)findViewById(R.id.opponent_editText);
-                String opp = et.getText().toString();
+        EditText gjv = (EditText) findViewById(R.id.gjv_time_editText);
+        EditText bjv = (EditText) findViewById(R.id.bjv_time_editText);
+        EditText gv = (EditText) findViewById(R.id.gv_time_editText);
+        EditText bv = (EditText) findViewById(R.id.bv_time_editText);
+        gjv.setOnFocusChangeListener(this);
+        bjv.setOnFocusChangeListener(this);
+        gv.setOnFocusChangeListener(this);
+        bv.setOnFocusChangeListener(this);
+        Button save = (Button)findViewById(R.id.game_save_button);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePicker dp = (DatePicker) findViewById(R.id.datePicker);
+                date = getDateFromDatePicker(dp);
+                EditText opp = (EditText) findViewById(R.id.opponent_editText);
+                String opponent = opp.getText().toString();
                 RadioGroup rgroup = (RadioGroup) findViewById(R.id.location_group);
-                int loc = rgroup.getCheckedRadioButtonId();
-                RadioButton locButton = (RadioButton) findViewById(loc);
-                String location = locButton.getText().toString();
-                et = (EditText)findViewById(R.id.location_editText);
-                String location_name = et.getText().toString();
-                et = (EditText)findViewById(R.id.gjv_time_editText);
-                String gjv_time = et.getText().toString();
-                et = (EditText)findViewById(R.id.bjv_time_editText);
-                String bjv_time = et.getText().toString();
-                et = (EditText)findViewById(R.id.gvar_time_editText);
-                String gv_time = et.getText().toString();
-                et = (EditText)findViewById(R.id.bvar_time_editText);
-                String bv_time = et.getText().toString();
-
+                int venue = rgroup.getCheckedRadioButtonId();
+                RadioButton venueButton = (RadioButton) findViewById(venue);
+                String venue_name = venueButton.getText().toString();
+                EditText location = (EditText) findViewById(R.id.location_editText);
+                String loc = location.getText().toString();
 
                 GameModel newGame = new GameModel();
-                newGame.setGame_date(db.GetDate(date));
-                newGame.setOpp_name(opp);
-                newGame.setLocation(location_name);
-                newGame.setVenue(location);
-                newGame.setGirls_jv(db.GetDate(gjv_time));
-                newGame.setGirls_v(db.GetDate(bjv_time));
-                newGame.setBoys_jv(db.GetDate(gv_time));
-                newGame.setBoys_v(db.GetDate(bv_time));
+                newGame.setGame_date(date);
+                newGame.setGirls_jv(times[0]);
+                newGame.setBoys_jv(times[1]);
+                newGame.setGirls_v(times[2]);
+                newGame.setBoys_v(times[3]);
+                newGame.setOpp_name(opponent);
+                newGame.setVenue(venue_name);
+                newGame.setLocation(loc);
+
+
 
                 db.open();
-
                 db.createGame(newGame);
                 db.close();
-
             }
         });
 
+    }
 
+    @Override
+    public void onFocusChange(View view, boolean b) {
 
+        if (b){
+        switch(view.getId()) {
+            case R.id.gjv_time_editText:
+                getTimePicker(view, 0);
+                break;
+
+            case R.id.bjv_time_editText:
+                getTimePicker(view, 1);
+                break;
+
+            case R.id.gv_time_editText:
+                getTimePicker(view, 2);
+                break;
+
+            case R.id.bv_time_editText:
+                getTimePicker(view, 3);
+                break;
+
+            default:
+                break;
+
+        }
+        }
+    }
+
+    public void getTimePicker (View v, int i) {
+        final EditText et = (EditText) findViewById(v.getId());
+        final int time_code = i;
+        int hour = 16;
+        int minute = 00;
+        TimePickerDialog timePicker = new TimePickerDialog(this, 2, new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                DateFormat df = new SimpleDateFormat("h:mm");
+                times[time_code] = new Date();
+                times[time_code].setMinutes(selectedMinute);
+                times[time_code].setHours(selectedHour);
+                et.setText(df.format(times[time_code]));
+             }
+        }, hour, minute, false);
+        timePicker.setTitle("Select Time");
+        timePicker.show();
 
     }
 
+    public static java.util.Date getDateFromDatePicker(DatePicker datePicker){
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        return calendar.getTime();
+    }
 
 }
